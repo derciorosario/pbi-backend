a1aceece0ee283ce26e9ab7009620a11bc80aa78
derciorosario <derciorosario55@gmail.com>
2025-09-11 13:26:26 +0200

    .

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.use((err, req, res, next) => {
 // ---------------------------
 // Start Server + DB
 // ---------------------------
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 5000;
 
 const { seedIfEmpty } = require("./src/utils/seed");
 const seedAll = require("./src/seeds/seedAll");
